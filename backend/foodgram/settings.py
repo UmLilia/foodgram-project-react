6180cf19760b4d6020a50743562b50821e4e7864
UmLilia <umanets.lilia@gmail.com>
2024-05-07 18:49:35 +0300

    a

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = bool(os.getenv('DEBUG') == 'True')
 
-ALLOWED_HOSTS = [os.getenv('HOST')]
+ALLOWED_HOSTS = ['158.160.87.131', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
